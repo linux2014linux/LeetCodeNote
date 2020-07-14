47e1010d68de0faafbfb67fb64fff4763b8a45d0
liulei1 <liulei1@bigo.sg>
2020-07-14 11:29:56 +0800

    5-最长回文子串补充暴力和动态规划解法

--- Java/src/main/java/Java/Solution/S0005/S0005.java
+++ Java/src/main/java/Java/Solution/S0005/S0005.java
@@ -1,9 +1,56 @@
 package Java.Solution.S0005;
 
+import java.util.Arrays;
+
 public class S0005 {
   public String longestPalindrome(String s) {
-    if (s == null || s.length() == 0) {
-      return "";
+    return way3(s);
+  }
+
+  /**
+   * 暴力方法, 找出每个子串, 依次判断是否是回文串并找出长度最大的
+   * @param s
+   * @return
+   */
+  public String way1(String s) {
+
+    if (s == null || s.length() < 2) {
+      return null;
+    }
+
+    // s.charAt每次都会检查越界, 浪费时间
+    char[] chs = s.toCharArray();
+    int length = s.length();
+    int startOfMax = 0, maxLen = 1;
+    for (int i = 0; i < length; i++) {
+      for (int j = i + 1; j < length; j++) {
+        int curLen = j - i + 1;
+        if (curLen > maxLen && validate(chs, i, j)) {
+          maxLen = curLen;
+          startOfMax = i;
+        }
+      }
+    }
+
+    return s.substring(startOfMax, startOfMax + maxLen);
+  }
+  private boolean validate(char[] s, int start, int end) {
+    while (start < end) {
+      if (s[start++] != s[end--]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * 以每个字符作为中心点对折 时间复杂度最低, 但细节较多
+   * @param s
+   * @return
+   */
+  public String way2(String s) {
+    if (s == null || s.length() < 2) {
+      return null;
     }
 
     int allLength = s.length();
@@ -57,5 +104,44 @@ public class S0005 {
       halfLength = maxLength / 2;
       return s.substring(maxMidPos - halfLength + 1, maxMidPos + halfLength + 1);
     }
+  }
+
+  /**
+   * 动态规划
+   * @param s
+   * @return
+   */
+  public String way3(String s) {
+    if (s == null || s.length() < 2) {
+      return null;
+    }
+
+    char[] chs = s.toCharArray();
+    int length = s.length();
+    boolean[][] dp = new boolean[length][length];
+    for (int i = 0; i < length; i++) {
+      Arrays.fill(dp[i], false);
+    }
+
+    for (int i = 0; i < length; i++) {
+      dp[i][i] = true;
+    }
+
+    int maxLen = 1;
+    int startOfMax = 0;
+    for (int j = 1; j < length; j++) {
+      for (int i = 0; i < j; i++) {
+        dp[i][j] = chs[i] == chs[j] && (j - i < 3 || dp[i + 1][j - 1]);
+        if (dp[i][j]) {
+          int curLen = j - i + 1;
+          if (curLen > maxLen) {
+            maxLen = curLen;
+            startOfMax = i;
+          }
+        }
+      }
+    }
+
+    return s.substring(startOfMax, startOfMax + maxLen);
   }
 }
--- Java/src/test/java/Java/Solution/S0005/S0005Test.java
+++ Java/src/test/java/Java/Solution/S0005/S0005Test.java
@@ -1,5 +1,6 @@
 package Java.Solution.S0005;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class S0005Test {
@@ -11,38 +12,34 @@ public class S0005Test {
     String output = null;
 
     output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
+    Assert.assertTrue(output == null);
 
     input = "";
     output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
-
-    input = "a";
-    output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
+    Assert.assertTrue(output == null);
 
     input = "aa";
     output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
+    Assert.assertTrue("aa".equals(output));
 
     input = "aaa";
     output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
+    Assert.assertTrue("aaa".equals(output));
 
     input = "abbbaadbdaa";
     output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
+    Assert.assertTrue("aadbdaa".equals(output));
 
     input = "abc";
     output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
+    Assert.assertTrue("a".equals(output) || "b".equals(output) || "c".equals(output));
 
     input = "aba";
     output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
+    Assert.assertTrue("aba".equals(output));
 
     input = "abcbadabc";
     output = s0005.longestPalindrome(input);
-    System.out.println(String.format("output:%s", output));
+    Assert.assertTrue("cbadabc".equals(output));
   }
 }
